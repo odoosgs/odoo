750f5d3fb0c936f31e5c351cdcc444ea6f3ef287
Luis Garcia <36515529+xentinell@users.noreply.github.com>
2026-01-13 11:57:41 -0600

    Update portal.py

--- custodia_logistica/controllers/portal.py
+++ custodia_logistica/controllers/portal.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+from odoo import http, fields
 from odoo.http import request
+from datetime import datetime
 
 class CustodiaPortal(http.Controller):
 
@@ -43,28 +44,39 @@ class CustodiaPortal(http.Controller):
     def solicitar_submit(self, **post):
         company = request.env.user.partner_id.commercial_partner_id
         try:
+            # Convertir fecha/hora del input HTML5 (ejemplo: 2026-01-29T14:51)
+            start_dt = False
+            if post.get('start_datetime'):
+                try:
+                    start_dt = datetime.strptime(post['start_datetime'], "%Y-%m-%dT%H:%M")
+                except Exception:
+                    # fallback: usar el parser de Odoo
+                    start_dt = fields.Datetime.from_string(post['start_datetime'])
+
             vals = {
                 'partner_id': company.id,
                 'contact_id': int(post['contact_id']),
                 'carrier_id': int(post['carrier_id']),
                 'ruta_id': int(post['ruta_id']),
-                'start_datetime': post['start_datetime'],
+                'start_datetime': start_dt,
                 'nivel_seguridad': post['nivel_seguridad'],
                 'load_id': post['load_id'],
-                'tipo_unidad': post['tipo_unidad'],
-                'placas': post['placas'],
-                'operador1_nombre': post['operador1_nombre'],
-                'operador1_licencia': post['operador1_licencia'],
-                'transporte': post['transporte'],
+                'tipo_unidad': post.get('tipo_unidad'),
+                'placas': post.get('placas'),
+                'transporte': post.get('transporte'),
+                'operador1_nombre': post.get('operador1_nombre'),
+                'operador1_licencia': post.get('operador1_licencia'),
                 'operador2_nombre': post.get('operador2_nombre') or False,
                 'operador2_licencia': post.get('operador2_licencia') or False,
-                'tel_monitoreo_1': post['tel_monitoreo_1'],
+                'tel_monitoreo_1': post.get('tel_monitoreo_1'),
                 'tel_monitoreo_2': post.get('tel_monitoreo_2') or False,
                 'comentarios_cliente': post.get('comentarios_cliente') or False,
                 'state': 'solicitado',
             }
+
             service = request.env['custodia.service'].sudo().create(vals)
             return request.redirect('/mis-servicios/%s' % service.id)
+
         except Exception as e:
             # Si hay error, re-renderizamos el formulario con mensaje
             carriers = request.env['custodia.carrier'].sudo().search([])
