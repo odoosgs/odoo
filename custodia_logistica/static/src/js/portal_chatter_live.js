5549935a7cd693b17a14c2485682a75fde64537a
Luis Garcia <36515529+xentinell@users.noreply.github.com>
2026-02-16 13:53:08 -0600

    Update portal_chatter_live.js

--- custodia_logistica/static/src/js/portal_chatter_live.js
+++ custodia_logistica/static/src/js/portal_chatter_live.js
@@ -3,30 +3,45 @@
 import { publicWidget } from "@web/legacy/js/public/public_widget";
 
 publicWidget.registry.CustodiaChatterLive = publicWidget.Widget.extend({
-    selector: '.o_portal_chatter_container', // Selector nativo más seguro
-    
+    selector: '#service_chatter',
+
+    /**
+     * @override
+     */
     start: function () {
         this.resId = parseInt(this.$el.attr('data-res-id'));
-        if (this.resId && this.call) {
-            this.call('bus_service', 'addChannel', `custodia_service_${this.resId}`);
-            this.call('bus_service', 'subscribe', `custodia_service_${this.resId}`, this._onNotification.bind(this));
+        this.resModel = this.$el.attr('data-res-model');
+        
+        if (this.resId && this.resModel === 'custodia.service') {
+            // Verificamos que el servicio de bus esté disponible antes de llamar
+            if (this.call) {
+                this.call('bus_service', 'addChannel', `custodia_service_${this.resId}`);
+                this.call('bus_service', 'subscribe', `custodia_service_${this.resId}`, this._onNotification.bind(this));
+            }
         }
         return this._super.apply(this, arguments);
     },
 
+    /**
+     * Manejador de notificaciones del bus
+     */
     _onNotification: function (payload) {
-        // Verificamos que el payload tenga la estructura esperada
         if (payload && payload.type === 'mail.record/insert' && payload.res_id === this.resId) {
+            // No refrescar si el usuario tiene el foco en el editor para evitar que se le borre lo que escribe
             const $composer = this.$('.o_portal_chatter_composer_body');
-            if ($composer.length && !$composer.is(':focus')) {
+            if ($composer.length === 0 || !$composer.is(':focus')) {
                 this._refreshChatter();
             }
         }
     },
 
+    /**
+     * Recarga parcial del contenedor de mensajes
+     */
     _refreshChatter: function() {
         const $messages = this.$('.o_portal_chatter_messages');
         if ($messages.length) {
+            // Refresca solo la sección de mensajes usando el endpoint actual
             $messages.load(window.location.href + " .o_portal_chatter_messages > *");
         }
     }
