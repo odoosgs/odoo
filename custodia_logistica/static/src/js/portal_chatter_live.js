04e6ebc52d7347cca12b76cf8417a8eb644981f1
Luis Garcia <36515529+xentinell@users.noreply.github.com>
2026-02-16 13:25:32 -0600

    Update portal_chatter_live.js

--- custodia_logistica/static/src/js/portal_chatter_live.js
+++ custodia_logistica/static/src/js/portal_chatter_live.js
@@ -1,39 +1,33 @@
 /** @odoo-module **/
 
 import { publicWidget } from "@web/legacy/js/public/public_widget";
-import { bus } from 'web.core';
 
 publicWidget.registry.CustodiaChatterLive = publicWidget.Widget.extend({
-    selector: '.o_portal_chatter', // Se cuelga del chatter estándar del portal
+    selector: '#service_chatter', // Usamos el ID que pusimos en el XML
     
     start: function () {
-        const self = this;
-        // Obtenemos el ID del servicio y el modelo de los datos del DOM
-        this.resId = parseInt($("span[data-res-id]").data('res-id'));
-        this.resModel = 'custodia.service';
-
+        this.resId = parseInt(this.$el.data('res-id'));
         if (this.resId) {
-            // Nos suscribimos al canal de notificaciones de este registro específico
+            // Usamos el servicio de bus de Odoo de forma segura
             this.call('bus_service', 'addChannel', `custodia_service_${this.resId}`);
-            this.call('bus_service', 'onNotification', this, this._onNotification);
+            this.call('bus_service', 'subscribe', `custodia_service_${this.resId}`, this._onNotification.bind(this));
         }
         return this._super.apply(this, arguments);
     },
 
-    _onNotification: function (notifications) {
-        const self = this;
-        notifications.forEach(notif => {
-            // Si llega una notificación de tipo 'message_post' para este servicio
-            if (notif.type === 'mail.record/insert' && notif.payload.res_id === self.resId) {
-                // Refrescamos solo el widget del chatter sin recargar toda la web
-                self._refreshChatter();
+    _onNotification: function (payload) {
+        if (payload.type === 'mail.record/insert' && payload.res_id === this.resId) {
+            // Solo refrescamos si el usuario no está escribiendo
+            if (!this.$('.o_portal_chatter_composer_body').is(':focus')) {
+                this._refreshChatter();
             }
-        });
+        }
     },
 
     _refreshChatter: function() {
-        // Ejecuta la recarga parcial del contenedor de mensajes
-        $(".o_portal_chatter_messages").load(window.location.href + " .o_portal_chatter_messages > *");
-        console.log("Chatter actualizado en tiempo real para el folio de custodia.");
+        const $messages = this.$('.o_portal_chatter_messages');
+        if ($messages.length) {
+            $messages.load(window.location.href + " .o_portal_chatter_messages > *");
+        }
     }
 });
