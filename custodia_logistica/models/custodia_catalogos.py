fb5ac263963a7b4c2c416a615a6e8ab40611e05e
Luis Garcia <36515529+xentinell@users.noreply.github.com>
2026-03-04 13:48:57 -0600

    Update custodia_catalogos.py

--- custodia_logistica/models/custodia_catalogos.py
+++ custodia_logistica/models/custodia_catalogos.py
@@ -8,9 +8,11 @@ class CustodiaRutaMaestra(models.Model):
     name = fields.Char(string="Nombre de Ruta", required=True)
 
 class CustodiaPuntoOperativo(models.Model):
-    _name = 'custodia.punto.operativo' # <--- Cambio de nombre técnico
+    _name = 'custodia.punto.operativo'
     _description = 'Puntos Operativos (Bodegas del Excel)'
-    
+    _order = 'sequence, id' # Ordenar por secuencia
+
     name = fields.Char(string="Nombre del Punto", required=True)
     latitude = fields.Float(string="Latitud")
     longitude = fields.Float(string="Longitud")
+    sequence = fields.Integer(string="Secuencia", default=10) # <--- AGREGAR ESTA LÍNEA
