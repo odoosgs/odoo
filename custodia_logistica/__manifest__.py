dfc7b9ae7afb9f9cd19b82289c9bfe3c247b2295
Luis Garcia <36515529+xentinell@users.noreply.github.com>
2026-04-16 14:50:14 -0600

    Update __manifest__.py

--- custodia_logistica/__manifest__.py
+++ custodia_logistica/__manifest__.py
@@ -31,6 +31,7 @@
         'views/custodia_catalogos_views.xml', # <--- Nuevo        
         'views/custodia_carrier_views.xml',
         'views/custodia_ruta_views.xml',
+        'views/custodia_views.xml',
 
         # Servicio principal
         'views/custodia_service_views.xml',
