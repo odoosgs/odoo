4abeceb9712f769d1414816510f44cd9ab504879
Luis Garcia <36515529+xentinell@users.noreply.github.com>
2026-05-13 20:15:04 -0600

    Update __manifest__.py

--- sgs_custody_perdiem/__manifest__.py
+++ sgs_custody_perdiem/__manifest__.py
@@ -14,8 +14,8 @@ Módulo para administrar custodios de vehículos pesados, depósitos de viático
     'license': 'LGPL-3',
     'depends': ['base', 'web', 'mail', 'portal'],
     'data': [
-        'security/security.xml',
         'security/ir.model.access.csv',
+        'security/security.xml',
         'data/sequence.xml',
         'views/assets.xml',
         'views/menu.xml',
